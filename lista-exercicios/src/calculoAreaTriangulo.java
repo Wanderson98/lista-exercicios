af4091ec8c2b303e3cc36db52678995b7c3ba75f
Wanderson Andrade <wmandrade81@gmail.com>
2021-09-13 14:54:59 -0300

    atividade1

--- lista-exercicios/src/calculoAreaTriangulo.java
+++ lista-exercicios/src/calculoAreaTriangulo.java
@@ -4,28 +4,31 @@ public class calculoAreaTriangulo {
     public static void main(String[] args) throws Exception {
         Scanner ler = new Scanner(System.in);
 
-        float medida1, medida2, medida3;
+        double medida1, medida2, medida3;
 
         System.out.println("Digite as areas do Triangulo1");
         System.out.print("Medida 1: ");
-        medida1 = ler.nextFloat();
+        medida1 = ler.nextDouble();
         System.out.print("Medida 2: ");
-        medida2 = ler.nextFloat();
+        medida2 = ler.nextDouble();
         System.out.print("Medida 3: ");
-        medida3 = ler.nextFloat();
+        medida3 = ler.nextDouble();
 
-        float areaTriangulo1 = (medida1 + medida2 + medida3) / 2;
+        double Triangulo1 = (medida1 + medida2 + medida3) / 2;
+        double areaTriangulo1 = Math.sqrt(Triangulo1 * (Triangulo1 - medida1) * (Triangulo1 -medida2) * (Triangulo1 - medida3) );
 
         System.out.println("Digite as areas do Triangulo2");
         System.out.print("Medida 1: ");
-        medida1 = ler.nextFloat();
+        medida1 = ler.nextDouble();
         System.out.print("Medida 2: ");
-        medida2 = ler.nextFloat();
+        medida2 = ler.nextDouble();
         System.out.print("Medida 3: ");
-        medida3 = ler.nextFloat();
+        medida3 = ler.nextDouble();
 
         
-        float areaTriangulo2 = (medida1 + medida2 + medida3) / 2;
+        double Triangulo2 = (medida1 + medida2 + medida3) / 2;
+        double areaTriangulo2 = Math.sqrt(Triangulo2 * (Triangulo2 - medida1) * (Triangulo2 -medida2) * (Triangulo2 - medida3) );
+
         
         System.out.printf("Area do trinagulo 1: %.2f%n", areaTriangulo1);
         System.out.printf("Area do trinagulo 2: %.2f%n", areaTriangulo2);
@@ -34,14 +37,10 @@ public class calculoAreaTriangulo {
                 System.out.print("Area maior: Triangulo 1");
             } else if(areaTriangulo1 == areaTriangulo2) { 
                 System.out.print("Area maior: Os dois triangulos possuem a mesma area !");
-            } else{
+            }else{
                 System.out.print("Area maior: Triangulo 2");
-            }      
-
-     
+            }  
             ler.close();
-
-        
     } 
     
 }
