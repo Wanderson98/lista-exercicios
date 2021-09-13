9f011a0f8c76243dd2afac0aa82cf4b7e4af2586
Wanderson Andrade <wmandrade81@gmail.com>
2021-09-13 15:17:08 -0300

    atividadeComOrientacai

--- lista-exercicios/src/calculoAreaTriangulo.java
+++ lista-exercicios/src/calculoAreaTriangulo.java
@@ -3,42 +3,40 @@ public class calculoAreaTriangulo {
 
     public static void main(String[] args) throws Exception {
         Scanner ler = new Scanner(System.in);
-
-        double medida1, medida2, medida3;
-
+        //criação de novos objetos para usar os atributos da entidade triangulo
+        entidadeTrinagulo x, y;
+        x = new entidadeTrinagulo();
+        y = new entidadeTrinagulo();
+        //leitura dos dados
         System.out.println("Digite as areas do Triangulo1");
         System.out.print("Medida 1: ");
-        medida1 = ler.nextDouble();
+        x.medida1 = ler.nextDouble();
         System.out.print("Medida 2: ");
-        medida2 = ler.nextDouble();
+        x.medida2 = ler.nextDouble();
         System.out.print("Medida 3: ");
-        medida3 = ler.nextDouble();
-
-        double Triangulo1 = (medida1 + medida2 + medida3) / 2;
-        double areaTriangulo1 = Math.sqrt(Triangulo1 * (Triangulo1 - medida1) * (Triangulo1 -medida2) * (Triangulo1 - medida3) );
+        x.medida3 = ler.nextDouble();
+        //atribuindo o resultado do calculo da area feito no metodo da entidade triangulo
+        double areaTriangulo1 = x.calcularArea();
 
         System.out.println("Digite as areas do Triangulo2");
         System.out.print("Medida 1: ");
-        medida1 = ler.nextDouble();
+        y.medida1 = ler.nextDouble();
         System.out.print("Medida 2: ");
-        medida2 = ler.nextDouble();
+        y.medida2 = ler.nextDouble();
         System.out.print("Medida 3: ");
-        medida3 = ler.nextDouble();
-
-        
-        double Triangulo2 = (medida1 + medida2 + medida3) / 2;
-        double areaTriangulo2 = Math.sqrt(Triangulo2 * (Triangulo2 - medida1) * (Triangulo2 -medida2) * (Triangulo2 - medida3) );
-
-        
+        y.medida3 = ler.nextDouble();
+        //as mesmas operações em cima agora para o triangulo 2
+        double areaTriangulo2 = y.calcularArea();
+        //Impressão das areas calculadas
         System.out.printf("Area do trinagulo 1: %.2f%n", areaTriangulo1);
         System.out.printf("Area do trinagulo 2: %.2f%n", areaTriangulo2);
-
+        //logica para ver qual area é maior e ser imprimida para o usuário  
             if (areaTriangulo1 > areaTriangulo2){
                 System.out.print("Area maior: Triangulo 1");
             } else if(areaTriangulo1 == areaTriangulo2) { 
                 System.out.print("Area maior: Os dois triangulos possuem a mesma area !");
             }else{
-                System.out.print("Area maior: Triangulo 2");
+                System.out.println("Area maior: Triangulo 2");
             }  
             ler.close();
     } 
---
+++ lista-exercicios/src/entidadeTrinagulo.java
@@ -0,0 +1,14 @@
+public class entidadeTrinagulo {
+    //dados da entidade triangulo
+    public double medida1;
+    public double medida2;
+    public double medida3;
+
+    //metodo para calculo da area do triangulo
+    public double calcularArea(){
+        double p = (medida1 + medida2 + medida3) / 2;
+        return  Math.sqrt(p * (p - medida1) * (p - medida2) * (p - medida3) );
+    
+    }
+    
+}
